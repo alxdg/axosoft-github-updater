5319f3be72ed38ea88c79ea4045eb096dab5daa8
Alex Garcia <alxdg@hotmail.com>
2020-01-28 21:53:42 -0600

    [axof: 1] Added index files

---
+++ src/services/domains/axosoft/index.s.ts
@@ -0,0 +1,2 @@
+export { default as Axosoft } from './Axosoft';
+export { default as AxosoftService } from './AxosoftService';
---
+++ src/services/domains/github/index.d.ts
@@ -0,0 +1,1 @@
+export { default as GithubService } from './GithubService';
